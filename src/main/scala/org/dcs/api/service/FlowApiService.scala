873c29402d6f7e796b14e0d27c807a0ef884b57a
Cherian Mathew <cmathew@chakravyu.org>
2016-06-08 23:09:07 +0200

    Add Flow Api service and model classes

--- src/main/scala/org/dcs/api/service/FlowApiService.scala
+++ src/main/scala/org/dcs/api/service/FlowApiService.scala
@@ -6,12 +6,42 @@ import scala.beans.BeanProperty
   * Created by cmathew on 05/06/16.
   */
 
-case class Processor(@BeanProperty var pType: String,
-                     @BeanProperty var description: String,
-                     @BeanProperty var tags: List[String]) {
+case class Connection(@BeanProperty var id: String) {
+  def this() = this("")
+}
+
+case class FlowInstance(@BeanProperty var id: String,
+                        @BeanProperty var version: String,
+                        @BeanProperty var processors : List[ProcessorInstance],
+                        @BeanProperty var connections: List[Connection]) {
+  def this() = this("", "", Nil, Nil)
+}
+
+case class FlowTemplate(@BeanProperty var id: String) {
+  def this() = this("")
+}
+
+case class ProcessorInstance(@BeanProperty var id: String,
+                             @BeanProperty var status: String) {
+  def this() = this("", "")
+}
+
+case class ProcessorType(@BeanProperty var pType:String,
+                         @BeanProperty var description:String,
+                         @BeanProperty var tags: List[String]) {
   def this() = this("", "", Nil)
 }
 
 trait FlowApiService {
-  def processorTypes(tagFilter: String): List[String]
+  def instantiate(flowTemplateId:String, clientId: String):FlowInstance
+  def instance(flowInstanceId: String, clientId: String): FlowInstance
+  def remove(flowInstanceId: String, clientId: String): Boolean
 }
+
+trait ProcessorApiService {
+  def types(clientToken: String): List[ProcessorType]
+  def create(name: String, ptype: String, clientToken: String): ProcessorInstance
+  def start(processorId: String, clientToken: String): ProcessorInstance
+  def remove(processorId: String, clientToken: String): Boolean
+}
+
