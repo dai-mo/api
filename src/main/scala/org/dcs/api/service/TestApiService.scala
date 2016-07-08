ad107903997c9cd09ea6f9135626f330ada6560e
Cherian Mathew <cmathew@chakravyu.org>
2016-07-08 17:41:49 +0200

    Update test response for resource routing

--- src/main/scala/org/dcs/api/service/TestApiService.scala
+++ src/main/scala/org/dcs/api/service/TestApiService.scala
@@ -3,13 +3,11 @@ package org.dcs.api.service
 import scala.beans.BeanProperty
 
 
-case class TestResponse(@BeanProperty var response: String) {
-  def this() = this("")
+case class TestResponse(@BeanProperty var response: String,
+                        @BeanProperty var token: String) {
+  def this() = this("", "")
 }
 
 trait TestApiService {
-
   def hello(name: String): TestResponse
-
-  def error(eType: String): TestResponse
 }
