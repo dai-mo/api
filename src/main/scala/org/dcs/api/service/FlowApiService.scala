e6fe16bf0f4aad436b22505715b3e0d48517d766
Cherian Mathew <cmathew@chakravyu.org>
2016-08-22 15:17:32 +0200

    Add state to flow instance model object

--- src/main/scala/org/dcs/api/service/FlowApiService.scala
+++ src/main/scala/org/dcs/api/service/FlowApiService.scala
@@ -24,10 +24,11 @@ case class Connection(@BeanProperty var id: String,
 case class FlowInstance(@BeanProperty var id: String,
                         @BeanProperty var name: String,
                         @BeanProperty var nameId: String,
+                        @BeanProperty var state: String,
                         @BeanProperty var version: Long,
                         @BeanProperty var processors : List[ProcessorInstance],
                         @BeanProperty var connections: List[Connection]) {
-  def this() = this("", "", "", 0.0.toLong, Nil, Nil)
+  def this() = this("", "", "", "", 0.0.toLong, Nil, Nil)
 }
 
 case class FlowTemplate(@BeanProperty var id: String,
