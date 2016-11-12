78c5bb3122066b0b49fac32018dcfaa52ff931fc
Cherian Mathew <cmathew@chakravyu.org>
2016-11-12 10:11:46 +0100

    AL-28 : Change trigger response type to List[Either], Implement avro serialisation

--- src/main/scala/org/dcs/api/processor/RemoteProcessor.scala
+++ src/main/scala/org/dcs/api/processor/RemoteProcessor.scala
@@ -1,13 +1,33 @@
 package org.dcs.api.processor
 
+import java.util
 import java.util.{List => JavaList, Map => JavaMap}
 
+import org.dcs.commons.error.ErrorResponse
+import org.dcs.commons.serde.AvroImplicits._
+
+import org.apache.avro.Schema
 
 trait RemoteProcessor extends BaseProcessor
   with ProcessorDefinition {
 
-  def execute(input: Array[Byte], properties: JavaMap[String, String]): AnyRef
-  def trigger(input: Array[Byte], properties: JavaMap[String, String]): Array[Byte]
+  def execute(input: Array[Byte], properties: JavaMap[String, String]): List[Either[ErrorResponse, AnyRef]]
+
+  def trigger(input: Array[Byte], properties: JavaMap[String, String]): JavaList[Either[Array[Byte], Array[Byte]]] = {
+    val result = new util.ArrayList[Either[Array[Byte], Array[Byte]]]()
+    execute(input, properties).foreach{ out =>
+      result.add (
+        if (out.isLeft)
+          Left(out.left.get.serToBytes())
+        else {
+          Right(out.right.get.serToBytes(schema))
+        }
+      )
+    }
+    result
+  }
+
+  def schema(): Option[Schema]
 }
 
 trait ProcessorDefinition extends HasProperties
